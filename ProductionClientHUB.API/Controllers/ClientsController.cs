8812af8ddf0a51c090d8917688403319800bc21e
gildevson <gildevson@gmail.com>
2025-10-25 22:49:42 -0300

    Adicionando mais dados

---
+++ ProductionClientHUB.API/Controllers/ClientsController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProductionClientHUB.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ClientsController : ControllerBase
+{
+    [HttpPost]
+
+    public IActionResult Register()
+    {
+        return Ok();
+    }
+
+    [HttpPut]
+
+    public IActionResult Update()
+    {
+        return Ok();
+    }
+    
+    [HttpGet] 
+
+    public IActionResult GetAll()
+    {
+        return Ok();
+    }
+
+    [HttpGet("By-id")]
+
+    public IActionResult GetById(Guid id)
+    {
+        return Ok();
+    }
+
+    [HttpGet]
+    [Route("{id}")]
+
+
+    [HttpDelete]
+
+    public IActionResult Delete()
+    {
+        return Ok();
+    }   
+}
